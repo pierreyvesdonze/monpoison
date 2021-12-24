b8eb8f0098c3441dc85c3450d3dba995a14d4ce9
py <pyd3.14@gmail.com>
2021-12-24 14:27:20 +0100

    init + cru register + login

---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+var app = {
+
+    
+    init: function () {
+
+        /**
+        * *****************************
+        * L I S T E N E R S
+        * *****************************
+        */
+        
+       
+    },
+
+ 
+}
+
+document.addEventListener('DOMContentLoaded', app.init)
