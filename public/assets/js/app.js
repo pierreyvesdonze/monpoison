cbf00d625969370c4434e9fccbb19975664ca313
py <pyd3.14@gmail.com>
2022-01-11 11:07:17 +0100

    Add text to homepage + scroll to top button

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -10,10 +10,24 @@ var app = {
         */
         console.log('init');
         
-       
+        
+        /*Scroll to top button*/
+        var btn = $('#toTop');
+
+        $(window).scroll(function () {
+            if ($(window).scrollTop() > 300) {
+                btn.addClass('show');
+            } else {
+                btn.removeClass('show');
+            }
+        });
+
+        btn.on('click', function (e) {
+            e.preventDefault();
+            $('html, body').animate({ scrollTop: 0 }, '300');
+        });
     },
 
- 
 }
 
 document.addEventListener('DOMContentLoaded', app.init)
