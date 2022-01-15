a99fb5fbfd70ca3ea1ee536e913ee07adcbe8b5c
py <pyd3.14@gmail.com>
2022-01-15 02:03:07 +0100

    Sort posts by DESC

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -19,22 +19,19 @@ class PostRepository extends ServiceEntityRepository
         parent::__construct($registry, Post::class);
     }
 
-    // /**
-    //  * @return Post[] Returns an array of Post objects
-    //  */
-    /*
-    public function findByExampleField($value)
+     /**
+     * @return Post[] Returns an array of Post objects
+     */
+    public function findAllByDesc()
     {
         return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
+            ->orderBy('p.id', 'DESC')
             ->setMaxResults(10)
             ->getQuery()
             ->getResult()
         ;
     }
-    */
+
 
     /*
     public function findOneBySomeField($value): ?Post
