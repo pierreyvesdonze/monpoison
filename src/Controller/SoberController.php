dcd3a6155591e389101c4476448b7f50d8733557
py <pyd3.14@gmail.com>
2022-01-18 01:05:53 +0100

    Add security check on adding sober

--- src/Controller/SoberController.php
+++ src/Controller/SoberController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Sober;
 use App\Form\SoberType;
+use App\Repository\SoberRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,20 +17,31 @@ class SoberController extends AbstractController
         private  EntityManagerInterface $em
     ) {
     }
-    
+
     /**
      * @Route("/sobriete/ajouter", name="sober_add")
      */
-    public function addSober(Request $request): Response
+    public function addSober(
+        Request $request,
+        SoberRepository $soberRepository
+        ): Response
     {
         $form = $this->createForm(SoberType::class);
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formDate = $form->get('date')->getData();
+            if ($formDate = $soberRepository->findOneBy([
+                'date' => $formDate
+            ])) {
+                $this->addFlash('danger', 'Vous avez déjà été sobre ce jour là');
+
+                return $this->redirectToRoute('drink_calendar');
+            }
             $newSober = new Sober;
             $newSober->setUser($this->getUser());
             $newSober->setDate($form->get('date')->getData());
-            
+
             $this->em->persist($newSober);
             $this->em->flush();
 
@@ -42,5 +54,24 @@ class SoberController extends AbstractController
         return $this->render('sober/add.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/sobriete/retirer{soberId}", name="sober_remove")
+     */
+    public function removeSober(
+        $soberId,
+        SoberRepository $soberRepository
+    ) {
+        $soberDay = $soberRepository->findBy([
+            'id' => $soberId
+        ]);
+
+        $this->em->remove($soberDay[0]);
+        $this->em->flush();
+
+        $this->addFlash('success', 'Jour de sobriété retiré !');
+
+        return $this->redirectToRoute('drink_calendar');
     }
 }
