47ec78c2ca5e36a5ede3d9879fa1ebcd3cbafc4b
py <pyd3.14@gmail.com>
2022-01-19 13:28:32 +0100

    retest manifest for wpa

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,6 +3,17 @@ var app = {
     init: function () {
 
         /**
+       * *****************************
+       * SERVICE WORKER
+       * *****************************
+       */
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('sw.js')
+                .then(function () { console.log('Enregistrement reussi.') })
+                .catch(function (e) { console.error(e) });
+        }
+
+        /**
         * *****************************
         * L I S T E N E R S
         * *****************************
