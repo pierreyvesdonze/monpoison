275a67430a0d541227a0e2cf376bdb278a407478
py <pyd3.14@gmail.com>
2022-01-19 13:55:32 +0100

    test manifest

--- sw.js
+++ sw.js
@@ -1,10 +1,5 @@
-self.addEventListener('install', evt => {
-    console.log('install evt', evt);
-
-})
-
 self.addEventListener('fetch', evt => {
-    console.log('events captures : ');
-    console.log('fetch evt sur url', evt.request.url);
+    // console.log('events captures : ');
+    // console.log('fetch evt sur url', evt.request.url);
 
 }) 
