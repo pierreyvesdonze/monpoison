642522e26a9ba8737ba0a9d5e248dea8cda1e966
py <pyd3.14@gmail.com>
2022-01-19 14:01:13 +0100

    test manifest

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -7,11 +7,7 @@ var app = {
        * SERVICE WORKER
        * *****************************
        */
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('sw.js')
-                .then(function () { console.log('Enregistrement reussi.') })
-                .catch(function (e) { console.error(e) });
-        }
+  
 
         /**
         * *****************************
