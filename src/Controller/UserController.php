eefb774deece0f134f306324abe6522bfcd9ab90
py <pyd3.14@gmail.com>
2022-01-21 17:38:29 +0100

    Create UserStatsService and factorise UserController index

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\ArgumentUserRepository;
-use App\Repository\DrinkRepository;
-use App\Repository\SoberRepository;
+use App\Service\UserStatsService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,128 +18,38 @@ class UserController extends AbstractController
     /**
      * @Route("/user/profile", name="user_account")
      */
-    public function index(
-        DrinkRepository $drinkRepository,
-        SoberRepository $soberRepository,
-        ArgumentUserRepository $argRepo,
-    ): Response {
+    public function index(UserStatsService $userStatsService): Response {
         $user = $this->getUser();
-        $emConfig = $this->em->getConfiguration();
-        $emConfig->addCustomDatetimeFunction('DAY', 'DoctrineExtensions\Query\Mysql\Day');
 
-        $lastWeekDrinks    = $drinkRepository->findLastWeekDrinks($user);
-        $lastWeekCost      = $drinkRepository->findLastWeekCost($user);
-        $drinks            = $drinkRepository->findByUser($user);
-        $totalBeer         = $drinkRepository->findTotalBeer($user)[1];
-        $totalWine         = $drinkRepository->findTotalWine($user)[1];
-        $totalSpiritus     = $drinkRepository->findTotalSpiritus($user)[1];
-        $advantagesUser    = $argRepo->findAdvantagesByUser($user);
-        $inconvenientsUser = $argRepo->findInconvenientsByUser($user);
+        // Get total of drinks
+        $statsArray = $userStatsService->getDrinksStats($user);
 
-        $mondayDrinks    = (int)$drinkRepository->findByDay($user, 'Monday')[0][1];
-        $tuesdayDrinks   = (int)$drinkRepository->findByDay($user, 'Tuesday')[0][1];
-        $wednesdayDrinks = (int)$drinkRepository->findByDay($user, 'Wednesday')[0][1];
-        $thursdayDrinks  = (int)$drinkRepository->findByDay($user, 'Thursday')[0][1];
-        $fridayDrinks    = (int)$drinkRepository->findByDay($user, 'Friday')[0][1];
-        $saturdayDrinks  = (int)$drinkRepository->findByDay($user, 'Saturday')[0][1];
-        $sundayDrinks    = (int)$drinkRepository->findByDay($user, 'Sunday')[0][1];
-
-        // Sobers Days
-        $sobers          = count($soberRepository->findByUser($user));
+        // Get total of Sobers Days
+        $sobers = $userStatsService->getSobersdays($user);
 
         // Get dates sorted by ASC to calculate longest period of sobriety
-        $sobersDates = $soberRepository->findDatesByUser($user);
+        $periodMax = $userStatsService->getMaxSobrietyPeriod($user);
 
-        $period = 0;
-        $periodMax = 0;
-        $previousDay = null;
-        $calculPreviousDay = null;
-   
-        foreach($sobersDates as $soberDate) {
-            $calculPreviousDay = clone $soberDate['date'];
-            $calculPreviousDay->modify('-1 day');
+        // Get 7 days last drinks
+        $lastWeekDrinks = $userStatsService->getLastWeekDrinks($user);
 
-            if($calculPreviousDay == $previousDay) {
-                $period ++;
-            } else {
-                if ($period > $periodMax) {
-                    $periodMax = $period;
-                }
-                $period = 1;
-            }
-            $previousDay = $soberDate['date'];
-        }
+        // Get 7 days cost
+        $lastWeekCost = $userStatsService->getLastWeekCost($user);
 
-        if ($period > $periodMax) {
-            $periodMax = $period;
-        }
+        // Get all days of drinking day by day
+        $weekDrinks = $userStatsService->getDrinksByDay($user);
 
-        // Total drinks
-        $total = (int)$totalBeer + (int)$totalWine + (int)$totalSpiritus + (int)$sobers;
- 
-        // ratio of drinks & sobers
-        if (0 !== $total) {
-            if (null !== $sobers) {
-                $xSober    = ((int)$sobers * 100) / $total;
-            } else {
-                $xSober = 0;
-            }
-            if (null !== $totalBeer) {
-                $xBeer     = ((int)$totalBeer * 100) / $total;
-            } else {
-                $xBeer = 0;
-            }
-            if (null !== $totalWine) {
-                $xWine     = ((int)$totalWine * 100) / $total;
-            } else {
-                $xWine = 0;
-            }
-            if (null !== $totalSpiritus) {
-                $xSpiritus = ((int)$totalSpiritus * 100) / $total;
-            } else {
-                $xSpiritus = 0;
-            }
-        } else {
-            $xSober = $xBeer = $xWine = $xSpiritus = 0;
-        }
-
-        // ratio of argumentsUsers
-        $totalArgUsers = count($advantagesUser) + count($inconvenientsUser);
-
-        if (0 !== $totalArgUsers) {
-            if (null !== count($advantagesUser)) {
-                $xAdvantageUser = count($advantagesUser) * 100 / $totalArgUsers;
-            } else {
-                $xAdvantageUser = 0;
-            }
-            if (null !== count($inconvenientsUser)) {
-                $xInconvenientUser = count($inconvenientsUser) * 100 / $totalArgUsers;
-            } else {
-                $xInconvenientUser = 0;
-            }
-        } else {
-            $xAdvantageUser = $xInconvenientUser = 0;
-        }
+        // Get ratio of arguments & inconvenient 
+        $ratioAdvInconv = $userStatsService->getRatioAdvantageInconvenient($user);
 
         return $this->render('user/user.html.twig', [
             'user'           => $user,
-            'drinks'         => $drinks,
+            'statsArray'     => $statsArray,
             'sobers'         => $sobers,
             'lastWeekDrinks' => $lastWeekDrinks,
             'lastWeekCost'   => $lastWeekCost,
-            'xBeer'          => $xBeer,
-            'xWine'          => $xWine,
-            'xSpiritus'      => $xSpiritus,
-            'xSober'         => $xSober,
-            'xAdvantages'    => $xAdvantageUser,
-            'xInconvenients' => $xInconvenientUser,
-            'mondayDrinks'   => $mondayDrinks,
-            'tuesdayDrinks'  => $tuesdayDrinks,
-            'wednesdayDrinks'=> $wednesdayDrinks,
-            'thursdayDrinks' => $thursdayDrinks,
-            'fridayDrinks'   => $fridayDrinks,
-            'saturdayDrinks' => $saturdayDrinks,
-            'sundayDrinks'   => $sundayDrinks,
+            'ratioAdvInconv' => $ratioAdvInconv, 
+            'weekDrinks'     => $weekDrinks,
             'periodMax'      => $periodMax
         ]);
     }
---
+++ src/Service/UserStatsService.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\SoberRepository;
+use App\Repository\DrinkRepository;
+use App\Repository\ArgumentUserRepository;
+
+class UserStatsService
+{
+    public function __construct(
+        private SoberRepository $soberRepository,
+        private DrinkRepository $drinkRepository,
+        private ArgumentUserRepository $argRepo
+        )
+    {}
+
+    // Get dates sorted by ASC to calculate longest period of sobriety
+    public function getMaxSobrietyPeriod($user)
+    {
+        $sobersDates = $this->soberRepository->findDatesByUser($user);
+
+        $period = 0;
+        $periodMax = 0;
+        $previousDay = null;
+        $calculPreviousDay = null;
+
+        foreach ($sobersDates as $soberDate) {
+            $calculPreviousDay = clone $soberDate['date'];
+            $calculPreviousDay->modify('-1 day');
+
+            if ($calculPreviousDay == $previousDay) {
+                $period++;
+            } else {
+                if ($period > $periodMax) {
+                    $periodMax = $period;
+                }
+                $period = 1;
+            }
+            $previousDay = $soberDate['date'];
+        }
+
+        if ($period > $periodMax) {
+            $periodMax = $period;
+        }
+
+        return $periodMax;
+    }
+
+    // Get total of Sobers Days
+    public function getSobersdays($user)
+    {
+        return count($this->soberRepository->findByUser($user));
+    }
+
+    // Get Total of drinks with ratios
+    public function getDrinksStats($user)
+    {
+        $drinks        = $this->drinkRepository->findByUser($user);
+        $totalBeer     = $this->drinkRepository->findTotalBeer($user)[1];
+        $totalWine     = $this->drinkRepository->findTotalWine($user)[1];
+        $totalSpiritus = $this->drinkRepository->findTotalSpiritus($user)[1];
+
+        // Final object
+        $statsArray = [];
+
+        // Total sobers
+        $sobers = $this->getSobersdays($user);
+
+        // Total drinks
+        $totalDrinks = (int)$totalBeer + (int)$totalWine + (int)$totalSpiritus + (int)$sobers;
+
+        // ratio of drinks & sobers
+        if (0 !== $totalDrinks) {
+            if (null !== $sobers) {
+                $xSober    = ((int)$sobers * 100) / $totalDrinks;
+            } else {
+                $xSober = 0;
+            }
+            if (null !== $totalBeer) {
+                $xBeer     = ((int)$totalBeer * 100) / $totalDrinks;
+            } else {
+                $xBeer = 0;
+            }
+            if (null !== $totalWine) {
+                $xWine     = ((int)$totalWine * 100) / $totalDrinks;
+            } else {
+                $xWine = 0;
+            }
+            if (null !== $totalSpiritus) {
+                $xSpiritus = ((int)$totalSpiritus * 100) / $totalDrinks;
+            } else {
+                $xSpiritus = 0;
+            }
+        } else {
+            $xSober = $xBeer = $xWine = $xSpiritus = 0;
+        }
+
+        $statsArray['drinks']     = $drinks;
+        $statsArray['xSober']     = $xSober;
+        $statsArray['xBeer']      = $xBeer;
+        $statsArray['xWine']      = $xWine;
+        $statsArray ['xSpiritus'] = $xSpiritus;
+
+       return $statsArray;
+    }
+
+    // Get 7 days last drinks
+    public function getLastWeekDrinks($user)
+    {
+       return $this->drinkRepository->findLastWeekDrinks($user);
+    }
+
+    // Get 7 days cost
+    public function getLastWeekCost($user)
+    {
+        return $this->drinkRepository->findLastWeekCost($user);
+    }
+
+    // Get all days of drinking day by day
+    public function getDrinksByDay($user)
+    {
+        $weekDrinks = [];
+
+        $weekDrinks['monday']    = (int)$this->drinkRepository->findByDay($user, 'Monday')[0][1];
+        $weekDrinks['tuesday']   = (int)$this->drinkRepository->findByDay($user, 'Tuesday')[0][1];
+        $weekDrinks['wednesday'] = (int)$this->drinkRepository->findByDay($user, 'Wednesday')[0][1];
+        $weekDrinks['thursday']  = (int)$this->drinkRepository->findByDay($user, 'Thursday')[0][1];
+        $weekDrinks['friday']    = (int)$this->drinkRepository->findByDay($user, 'Friday')[0][1];
+        $weekDrinks['saturday']  = (int)$this->drinkRepository->findByDay($user, 'Saturday')[0][1];
+        $weekDrinks['sunday']    = (int)$this->drinkRepository->findByDay($user, 'Sunday')[0][1];
+
+        return $weekDrinks;
+    }
+
+    public function getRatioAdvantageInconvenient($user)
+    {
+        $advantagesUser    = $this->argRepo->findAdvantagesByUser($user);
+        $inconvenientsUser = $this->argRepo->findInconvenientsByUser($user);
+        $totalArgUsers = count($advantagesUser) + count($inconvenientsUser);
+
+        $ratioAdvantageInconvenient = [];
+
+        if (0 !== $totalArgUsers) {
+            if (null !== count($advantagesUser)) {
+                $xAdvantageUser = count($advantagesUser) * 100 / $totalArgUsers;
+            } else {
+                $xAdvantageUser = 0;
+            }
+            if (null !== count($inconvenientsUser)) {
+                $xInconvenientUser = count($inconvenientsUser) * 100 / $totalArgUsers;
+            } else {
+                $xInconvenientUser = 0;
+            }
+        } else {
+            $xAdvantageUser = $xInconvenientUser = 0;
+        }
+
+        $ratioAdvantageInconvenient['advantage'] = $xAdvantageUser;
+        $ratioAdvantageInconvenient['inconvenient'] = $xInconvenientUser;
+
+        return $ratioAdvantageInconvenient;
+    }
+}
