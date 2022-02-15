8bdc873210e209567361dc49ae035846e2b05331
py <pyd3.14@gmail.com>
2022-02-15 13:12:59 +0100

    V1.1

--- src/Repository/AlcoolRepository.php
+++ src/Repository/AlcoolRepository.php
@@ -18,33 +18,4 @@ class AlcoolRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Alcool::class);
     }
-
-    // /**
-    //  * @return Alcool[] Returns an array of Alcool objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('a.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Alcool
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
