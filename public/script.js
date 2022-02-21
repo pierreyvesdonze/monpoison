29c7e16bc6b81ea4f5d24148cab8c89ff26d78eb
py <pyd3.14@gmail.com>
2022-02-21 14:15:58 +0100

    fix bg on modal drinks calendar

--- public/script.js
+++ public/script.js
@@ -1,7 +1,6 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js')
         .then(function () {
-            
             console.log('Enregistrement reussi.')
         })
         //.catch(function (e) { console.error(e + '...') });
