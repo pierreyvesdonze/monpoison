a084b6ec8eda7646457edd22c98d3011f724cd3c
py <pyd3.14@gmail.com>
2022-03-30 11:29:24 +0200

    typo

--- src/Command/AutoSoberCommand.php
+++ src/Command/AutoSoberCommand.php
@@ -9,7 +9,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Security\Core\Security;
 
-class CreateUserCommand extends Command
+class AutoSoberCommand extends Command
 {
     private $soberService;
     private $security;
