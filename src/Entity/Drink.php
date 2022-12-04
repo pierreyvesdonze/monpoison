8e9911ca0baf79cdb96d9d3be067836a1b9c9bcd
py <pyd3.14@gmail.com>
2022-12-04 23:04:24 +0100

    add new propertie Word to Drink (just frondend)

--- src/Entity/Drink.php
+++ src/Entity/Drink.php
@@ -44,6 +44,11 @@ class Drink
      */
     private $cost;
 
+    /**
+     * @ORM\Column(type="string", length=64, nullable=true)
+     */
+    private $word;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -105,6 +110,18 @@ class Drink
     public function setCost(float $cost): self
     {
         $this->cost = $cost;
+
+        return $this;
+    }
+
+    public function getWord(): ?string
+    {
+        return $this->word;
+    }
+
+    public function setWord(?string $word): self
+    {
+        $this->word = $word;
 
         return $this;
     }
